3563fe10dbc51aa1bb40e5d185da137a2ac8b855
betomansur <email@example.com>
2021-12-02 10:51:44 -0300

    ff

---
+++ teste.py
@@ -0,0 +1,21 @@
+nome = []
+altura = []
+
+
+a = str(input('Qual o seu nome?'))
+aa = float(input('Qual a sua atlura?'))
+a.append(nome)
+aa.append(altura)
+
+b = str(input('Qual o seu nome?'))
+bb = float(input('Qual a sua atlura?'))
+b.append(nome)
+bb.append(altura)
+
+
+c = str(input('Qual o seu nome?'))
+cc = float(input('Qual a sua atlura?'))
+c.append(nome)
+cc.append(altura)
+
+maxi = max(altura)
